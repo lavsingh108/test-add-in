48f8d2d0e94045bfcbbb128e3941d2c25288dd14
lavsingh108 <lav.singh@smartblue.ai>
2026-03-26 01:19:45 +0530

    office boilerplate

--- taskpane.js
+++
@@ -1,8 +0,0 @@
-Office.onReady((info) => {
-    const status = document.getElementById("status");
-    if (info.host === Office.HostType.Outlook) {
-        status.innerText = "Success! Connected to Outlook.";
-    } else {
-        status.innerText = "Running outside of Outlook.";
-    }
-});
